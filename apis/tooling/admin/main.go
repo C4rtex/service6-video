7a7884eefee8794fc90849fb3e5af470c72521bf
Carlos Cardozo <carlos.jimenezc@cun.edu.co>
2025-08-03 10:45:16 -0500

    commit 25

--- apis/tooling/admin/main.go
+++ apis/tooling/admin/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	_ "embed"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/open-policy-agent/opa/rego"
 	"log"
 	"os"
 	"time"
@@ -86,8 +91,53 @@ func GenToken() error {
 		return fmt.Errorf("encoding to public file: %w", err)
 	}
 
+	var b bytes.Buffer
+	if err := pem.Encode(&b, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	// ------------------------------------------------------------------------
+
+	ctx := context.Background()
+	query := fmt.Sprintf("x = data.%s.%s", "ardan.rego", "auth")
+
+	q, err := rego.New(
+		rego.Query(query),
+		rego.Module("policy.rego", regoAuthentication),
+	).PrepareForEval(ctx)
+	if err != nil {
+		return err
+	}
+
+	input := map[string]any{
+		"Key":   b.String(),
+		"Token": str,
+		"ISS":   "service project",
+	}
+
+	results, err := q.Eval(ctx, rego.EvalInput(input))
+	if err != nil {
+		return fmt.Errorf("query: %w", err)
+	}
+
+	if len(results) == 0 {
+		return errors.New("no results")
+	}
+
+	result, ok := results[0].Bindings["x"].(bool)
+	if !ok || !result {
+		return fmt.Errorf("bindings results[%v] ok[%v]", results, ok)
+	}
+
+	fmt.Println("\nTOKEN VALIDATED")
+
 	return nil
 }
+
+// Core OPA policies.
+//
+//go:embed rego/authentication.rego
+var regoAuthentication string
 
 // GenKey creates an x509 private/public key for auth tokens.
 func GenKey() error {
